db27ec162960b543161115c9a1f312117bf6cc63
Jacinto Palma <jxcinto12@gmail.com>
2021-02-25 19:06:08 -0500

    Predictor function
    Obtain values from the from and validate them (license plate)

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,46 @@
+function prediction(){
+    //Gets the license plate data
+    let letters = document.getElementById('plateLetters').value
+    let numbers = document.getElementById('plateNumbers').value
+    
+    letters = letters.toUpperCase();
+
+    if (isNaN(letters)){
+        console.log('Correct input - just letters')
+        var licensePlate = letters
+    }else{
+        alert('Inorrect input - You must put letters on the first input');
+    }
+
+    if(isNaN(numbers)){
+        alert('You must put numbers on the second input') 
+    }else{
+        licensePlate = licensePlate+'-'+numbers
+        console.log('Correct input - just numbers')
+    
+    }
+    console.log('License Plate:' +licensePlate)
+    
+   
+
+}
+
+
 //Call the datepicker from the form
 const datefield = document.getElementById('datepicker')
-const picker = datepicker(datefield, {onSelect: instance => {
-    // Show which date was selected.
-    console.log("This is the selected date" + instance.dateSelected)
-  }
+
+const picker = datepicker(datefield, {
+
+    formatter: (input, date, instance) => {
+    
+    input.value = date.toDateString()
+    selection = input.value
+    console.log("This is the selected date......" + input.value)
+  },
+  
+  
 })
+
+
 
 
